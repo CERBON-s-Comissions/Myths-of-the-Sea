25348d226eec5427490b1f1bb914526633c13405
felip <felipecerboncinix@gmail.com>
2025-05-12 22:37:19 -0300

    Make variables final

--- Common/src/main/java/com/cerbon/myths_of_the_sea/integration/MTSIntegrations.java
+++ Common/src/main/java/com/cerbon/myths_of_the_sea/integration/MTSIntegrations.java
@@ -3,6 +3,6 @@ package com.cerbon.myths_of_the_sea.integration;
 import com.cerbon.cerbons_api.api.static_utilities.MiscUtils;
 
 public class MTSIntegrations {
-    public static boolean isTrinketsLoaded = MiscUtils.isModLoaded("trinkets");
-    public static boolean isCuriosLoaded = MiscUtils.isModLoaded("curios");
+    public static final boolean isTrinketsLoaded = MiscUtils.isModLoaded("trinkets");
+    public static final boolean isCuriosLoaded = MiscUtils.isModLoaded("curios");
 }
