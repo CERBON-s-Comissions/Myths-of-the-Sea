7747597e9b414c64737e5be64bd716dc28a2dc32
Felipe Cerboncini Cordeiro <felipecerboncinix@gmail.com>
2025-06-17 09:22:40 -0300

    Use DefaultedEntityGeoModel

--- Common/src/main/java/com/cerbon/myths_of_the_sea/entity/custom/abaia/AbaiaEntityModel.java
+++ Common/src/main/java/com/cerbon/myths_of_the_sea/entity/custom/abaia/AbaiaEntityModel.java
@@ -2,22 +2,11 @@ package com.cerbon.myths_of_the_sea.entity.custom.abaia;
 
 import com.cerbon.myths_of_the_sea.MythsOfTheSea;
 import net.minecraft.resources.ResourceLocation;
-import software.bernie.geckolib.model.GeoModel;
+import software.bernie.geckolib.model.DefaultedEntityGeoModel;
 
-public class AbaiaEntityModel extends GeoModel<AbaiaEntity> {
+public class AbaiaEntityModel extends DefaultedEntityGeoModel<AbaiaEntity> {
 
-    @Override
-    public ResourceLocation getModelResource(AbaiaEntity animatable) {
-        return new ResourceLocation(MythsOfTheSea.MOD_ID, "geo/abaia.geo.json");
-    }
-
-    @Override
-    public ResourceLocation getTextureResource(AbaiaEntity animatable) {
-        return new ResourceLocation(MythsOfTheSea.MOD_ID, "textures/entity/abaia.png");
-    }
-
-    @Override
-    public ResourceLocation getAnimationResource(AbaiaEntity animatable) {
-        return new ResourceLocation(MythsOfTheSea.MOD_ID, "animations/abaia.animation.json");
+    public AbaiaEntityModel() {
+        super(new ResourceLocation(MythsOfTheSea.MOD_ID, "abaia"));
     }
 }
