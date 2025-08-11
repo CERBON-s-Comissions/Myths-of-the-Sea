65c136ef67edcd847c7daa0ec06fc2c1c9a63c6c
Felipe Cerboncini Cordeiro <felipecerboncinix@gmail.com>
2025-08-11 13:48:35 -0300

    Fix Fabric crashing without trinkets installed

--- Fabric/src/main/java/com/cerbon/myths_of_the_sea/fabric/item/KrakenTentacleItemTrinkets.java
+++ Fabric/src/main/java/com/cerbon/myths_of_the_sea/fabric/item/KrakenTentacleItemTrinkets.java
@@ -4,6 +4,7 @@ import com.google.common.collect.Multimap;
 import dev.emi.trinkets.api.SlotReference;
 import dev.emi.trinkets.api.Trinket;
 import dev.emi.trinkets.api.TrinketsApi;
+import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.ai.attributes.Attribute;
 import net.minecraft.world.entity.ai.attributes.AttributeModifier;
@@ -11,17 +12,18 @@ import net.minecraft.world.item.ItemStack;
 
 import java.util.UUID;
 
-public class KrakenTentacleItemTrinkets extends KrakenTentacleItem implements Trinket {
+public class KrakenTentacleItemTrinkets extends KrakenTentacleItem {
 
     public KrakenTentacleItemTrinkets(Properties properties) {
         super(properties);
-        TrinketsApi.registerTrinket(this, this);
-    }
+        TrinketsApi.registerTrinket(this, new Trinket() {
 
-    @Override
-    public Multimap<Attribute, AttributeModifier> getModifiers(ItemStack stack, SlotReference slot, LivingEntity entity, UUID uuid) {
-        var modifiers = Trinket.super.getModifiers(stack, slot, entity, uuid);
-        this.getAttributes().forEach(modifiers::put);
-        return modifiers;
+            @Override
+            public Multimap<Attribute, AttributeModifier> getModifiers(ItemStack stack, SlotReference slot, LivingEntity entity, UUID uuid) {
+                var modifiers = Trinket.super.getModifiers(stack, slot, entity, uuid);
+                stack.getAttributeModifiers(EquipmentSlot.MAINHAND).forEach(modifiers::put);
+                return modifiers;
+            }
+        });
     }
 }
